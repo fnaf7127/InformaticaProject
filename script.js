fc402d52ba199dc8d6faab1ef9c638ef3cb5c638
fnaf7127 <gijs.veltmann@gmail.com>
2023-01-13 12:59:56 +0100

    CSS fix

--- script.js
+++ script.js
@@ -1,6 +1,48 @@
 const screen = document.querySelector('screen')
 
-let Total = 0
 let buffer = '0'
-let previous;
 
+function buttonClick(value) {
+    if (isNaN(value)) {
+        handleOperator(value)
+    } else {
+        handleNumber(value)
+    }
+    screen.innerText = buffer
+}
+
+function handleOperator(symbol) {
+    switch(symbol) {
+        case 'C':
+            buffer = '0'
+            break;
+        case '←':
+            buffer = substring(0, buffer.length - 1)
+            break;
+        case '+':
+        case '÷':
+        case '×':
+        case '−':
+            buffer += symbol
+            break;
+        case '=':
+            buffer = eval(buffer)
+    }
+}
+
+function handleNumber(number) {
+    if (buffer === '0') {
+        buffer = number
+    } else {
+        buffer += number
+    }
+}
+
+function initialize() {
+    document.querySelector('calc-buttons')
+    .addEventListener('click', function(event) {
+        buttonClick(event.target.innerText)
+    })
+}
+
+initialize()
